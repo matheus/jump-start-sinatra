0070e8823eb85e6d2ec3c93e0bbd4c1c08e89fab
matheus <matheusgodoy@me.com>
2017-11-05 14:55:24 -0200

    chapter 04

--- main.rb
+++ main.rb
@@ -1,7 +1,35 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 require 'slim'
 require 'sass'
 require './song'
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+get '/login' do
+    slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to ('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/set/:name' do
+  session[:name] = params[:name]
+end
+
+get '/get/hello' do
+  "Hello #{session[:name]}"
+end
 
 get('/styles.css'){ scss :styles}
 
